687e9ce44eab55d8a740952e79a0d22e1e023881
Harley McDowell <hmcdowell95@gmail.com>
2020-04-28 23:21:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,6 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
     user.save
-    binding.pry
     if user.username && user.password
       redirect "/login"
     else 
