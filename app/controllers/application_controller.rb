9e9fccd1d78cf8a04c6502dabfae01476ac654f1
Harley McDowell <hmcdowell95@gmail.com>
2020-04-28 23:08:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,8 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = user.id
       redirect "/account"
     else
-      
+      redirect "/failure"
+    end
   end
 
   get "/failure" do
